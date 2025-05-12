41a100b80e76ccfa62544606669b0c59b90435bf
lalit <shinkarlalit18@gmail.com>
2025-05-12 08:07:36 +0000

    frontend intergration with backend

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Frontend Loaded Successfully.");
+    fetch("/api/tasks")
+      .then(res => res.json())
+      .then(data => {
+        const list = document.getElementById('task-list');
+        data.forEach(task => {
+            const card = `
+              <div class="col-md-4 mb-3">
+                <div class="card p-3">
+                  <h5>${task.title}</h5>
+                  <p>${task.description}</p>
+                  <span class="badge ${task.completed ? 'bg-success' : 'bg-warning'}">
+                    ${task.completed ? 'Completed' : 'Pending'}
+                  </span>
+                </div>
+              </div>`;
+            list.innerHTML += card;
+        });
+      })
+      .catch(err => console.error("Error loading tasks:", err));
 });
 
